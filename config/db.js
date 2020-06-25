e8457fc8602858caffdbdf0b8198cfb9093db1d1
Rodrigo Rachadel <rachadel.rodrigo@gmail.com>
2020-06-25 19:42:48 -0300

    updating mongodb code

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production") {
-  module.exports = {mongoURI: "mongodb+srv://rachadel:ogaaaiib@blog-uuyep.mongodb.net/blog?retryWrites=true&w=majority"}
+  module.exports = {mongoURI: "mongodb+srv://rachadel:ogaaaiib@blog-uuyep.mongodb.net/blogapp?retryWrites=true&w=majority"}
 }else{
   module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
